1b754bc139685ebfea70b879f16aedbb4657d3e1
Alex Smith <alexsmith7192@gmail.com>
2021-02-24 19:23:09 -0300

    Resposta da questão 1

---
+++ introducao-a-javascript-e-logica-de-programacao_1/questao1.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 20;
+
+//adição
+soma = a + b;
+
+//subtração
+subtracao = b - a;
+
+//multiplicação
+multiplicacao = a * b;
+
+//divisão
+divisao = b / a;
+
+//módulo
+modulo = (a / b) % 2;
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
