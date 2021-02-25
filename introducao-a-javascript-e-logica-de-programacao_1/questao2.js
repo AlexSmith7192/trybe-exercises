34e38220fb85c9d86f1b0b0a2d09c6b86fb03f62
Alex Smith <alexsmith7192@gmail.com>
2021-02-25 16:02:54 -0300

    Resposta da questão 2

---
+++ introducao-a-javascript-e-logica-de-programacao_1/questao2.js
@@ -0,0 +1,10 @@
+let x = 7;
+let y = 7;
+
+if (x > y) {
+  console.log(x,' é maior que ',y);
+} else if (y > x) {
+  console.log(y,' é maior que ',x);
+} else {
+  console.log(x,' e ', y, ' são iguais');
+}
