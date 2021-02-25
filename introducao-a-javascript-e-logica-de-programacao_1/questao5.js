f2051d7d916785fa5f31eff631cf562caedc703e
Alex Smith <alexsmith7192@gmail.com>
2021-02-25 19:10:09 -0300

    Resposta da questão 5

---
+++ introducao-a-javascript-e-logica-de-programacao_1/questao5.js
@@ -0,0 +1,13 @@
+let a = 60;
+let b = 80;
+let c = 40;
+
+somaLados = (a + b + c)
+
+if (somaLados === 180) {
+  console.log(true)
+} else if (somaLados < 180 || somaLados > 180){
+  console.log(false)
+} else {
+  console.log('Algo de errado não está certo')
+}
