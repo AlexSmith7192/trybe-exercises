cb9b94a2b130c932647957e1e9d36e75ee69fa8d
Alex Smith <alexsmith7192@gmail.com>
2021-03-04 19:04:43 -0300

    2 - soluciona questao 2

--- introducao-a-javascript-e-logica-de-programacao_2/exercise-4-2.js
+++ introducao-a-javascript-e-logica-de-programacao_2/exercise-4-2.js
@@ -1,5 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//Primeira questão
 for (let indexNumber = 0; indexNumber < numbers.length; indexNumber += 1) {
   console.log(numbers[indexNumber]);
 }
+
+// questão
+let soma = 0;
+for (let indexNumber = 0; indexNumber < numbers.length; indexNumber += 1) {
+  soma = soma + numbers[indexNumber];
+}
+console.log(soma);
+
+
