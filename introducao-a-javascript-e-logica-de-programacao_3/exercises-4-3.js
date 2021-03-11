0a758b89c4d195021c5efd47803f2cccb195f284
Alex Smith <alexsmith7192@gmail.com>
2021-03-11 19:28:34 -0300

    Responde questao 2

--- introducao-a-javascript-e-logica-de-programacao_3/exercises-4-3.js
+++ introducao-a-javascript-e-logica-de-programacao_3/exercises-4-3.js
@@ -3,3 +3,9 @@ for (let index = 0; index < n; index += 1) {
   let asterisco = '*'
   console.log(asterisco.repeat(n));
 }
+
+n = 5;
+for (let index = 0; index <= n; index += 1) {
+  let asterisco = '*'
+  console.log(asterisco.repeat(index));
+}
