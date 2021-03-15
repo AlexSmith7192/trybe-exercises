4ae328ef5572b0e4b5c2819ac7f766fa3d28aefb
Alex Smith <alexsmith7192@gmail.com>
2021-03-15 18:19:26 -0300

    2 - Funcao que mostra o indice do maior numero de um array

--- introducao-a-javascript-e-logica-de-programacao_4/exercise-4-4-2.js
+++ introducao-a-javascript-e-logica-de-programacao_4/exercise-4-4-2.js
@@ -7,3 +7,15 @@ function verificaPalindromo (palavra) {
     return false;
   }
 }
+
+function indiceMaiorNumero (arrayNumbers) {
+  let maior = 0;
+  let indice = 0;
+  for (let indexNumbers = 0; indexNumbers < arrayNumbers.length; indexNumbers += 1) {
+    if (arrayNumbers[indexNumbers] > maior) {
+      maior = arrayNumbers[indexNumbers];
+      indice = indexNumbers;
+    }
+  }
+  return indice;;
+}
