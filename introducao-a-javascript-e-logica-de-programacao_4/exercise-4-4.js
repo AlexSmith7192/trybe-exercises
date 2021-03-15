1e269f3ddb918e312a06dd1660a88a83d6397064
Alex Smith <alexsmith7192@gmail.com>
2021-03-15 15:35:15 -0300

    Respondendo questao 5

--- introducao-a-javascript-e-logica-de-programacao_4/exercise-4-4.js
+++ introducao-a-javascript-e-logica-de-programacao_4/exercise-4-4.js
@@ -5,6 +5,14 @@ let info = {
   recorrente: 'Sim',
 };
 
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: 'Sim',
+};
+
+
 console.log('Bem vinda, ' + info.personagem);
 
 console.log(info)
@@ -15,4 +23,13 @@ for (keyInfo in info) {
 
 for (indexInfo in info) {
   console.log(info[indexInfo])
+}
+
+for (indexInfo in info) {
+  if (info[indexInfo] === info2[indexInfo]) {
+    console.log(info[indexInfo]);
+  }
+  if (info[indexInfo] != info2[indexInfo]) {
+    console.log(info[indexInfo],'e',info2[indexInfo]);
+  }
 }
