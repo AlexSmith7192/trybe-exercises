9f704c2868b01565854ae20b9c67daa20e639c59
AlexSmith7192 <alexsmith7192@gmail.com>
2021-04-26 18:56:31 -0300

    Adiciona comentários

--- introducao-a-javascript-es6-e-testes-unitarios.1/exercise-1.js
+++ introducao-a-javascript-es6-e-testes-unitarios.1/exercise-1.js
@@ -1,16 +1,18 @@
-// const testingScope = (escopo) => {
-//   if (escopo === true) {
-//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-//     ifScope = `ótimo, fui utilizada no escopo !`;
-//     console.log(ifScope);
-//   } else {
-//     let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-//     console.log(elseScope);
-//   }
-// }
+//Questão 1
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+    console.log(elseScope);
+  }
+}
 
-// testingScope(true);
+testingScope(true);
 
+//Questão 2
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 const sortOddsAndEvens = () => {
