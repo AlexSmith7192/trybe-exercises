b2879e7510abb9693e612a939ab20235d51539ac
AlexSmith7192 <alexsmith7192@gmail.com>
2021-04-26 18:53:18 -0300

    Resolve segunda questao

--- introducao-a-javascript-es6-e-testes-unitarios.1/exercise-1.js
+++ introducao-a-javascript-es6-e-testes-unitarios.1/exercise-1.js
@@ -1,12 +1,30 @@
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = `ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-    console.log(elseScope);
-  }
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//     ifScope = `ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
 }
 
-testingScope(true);
+const oddsAndEvensOrder = sortOddsAndEvens();
+console.log(`Array em ordem crescente: ${oddsAndEvensOrder}`);
+
+//Tive uma ajudinha do gabarito
