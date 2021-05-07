31fff9b8460536392caa08568f5becb71d5bc001
AlexSmith7192 <alexsmith7192@gmail.com>
2021-05-07 10:05:28 -0300

    Resolve exercicio 2 do dia 8.4

--- higher-order-functions-do-javascript-es6.4/exercise1.js
+++ higher-order-functions-do-javascript-es6.4/exercise1.js
@@ -9,5 +9,5 @@ const arrays = [
 function flatten() {
   return arrays.reduce((acc, object) => acc.concat(object));
 }
-console.log(flatten());
+
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
