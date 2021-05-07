cc487dd8ed150f4b1037d915e746146ce5cb2ac9
AlexSmith7192 <alexsmith7192@gmail.com>
2021-05-07 11:18:18 -0300

    Resolve exercicio 5 do dia 8.4

---
+++ higher-order-functions-do-javascript-es6.4/exercise5.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, value) => acc + value).toUpperCase().split('A').length - 1;
+}
+
+assert.deepStrictEqual(containsA(), 20);
